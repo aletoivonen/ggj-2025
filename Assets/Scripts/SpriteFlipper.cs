dbfdd85e6c680441f60350a06c598a1d3f24d93b
kallecollan <kalle.collan@zaibatsu.fi>
2025-01-25 17:47:38 +0200

    Flip player sprite based on direction

---
+++ Assets/Scripts/SpriteFlipper.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+namespace Zubble
+{
+    public class SpriteFlipper : MonoBehaviour
+    {
+        [SerializeField] private Rigidbody2D _rb;
+        [SerializeField] private SpriteRenderer _spriteRenderer;
+        void Update()
+        {
+            if (_rb.linearVelocity.x > 0)
+            {
+                _spriteRenderer.flipX = false;
+            }
+            else if (_rb.linearVelocity.x < 0)
+            {
+                _spriteRenderer.flipX = true;
+            }
+        }
+    }
+}
