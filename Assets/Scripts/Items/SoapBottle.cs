3663a1d5c885492bbd799c72eceabdf3adb505dd
TuomoO <tuomo.oksanen@zaibatsu.fi>
2025-01-25 18:31:58 +0200

    Delete on collide

--- Assets/Scripts/Items/SoapBottle.cs
+++ Assets/Scripts/Items/SoapBottle.cs
@@ -1,4 +1,3 @@
-using System;
 using UnityEngine;
 
 namespace Zubble.Items
@@ -9,29 +8,29 @@ namespace Zubble.Items
 
         private void Update()
         {
-            _child.Rotate(new Vector3(0.5f, 1, 0), Time.deltaTime * 50);
+            _child.Rotate(new Vector3(0f, 1.0f, 0.5f), Time.deltaTime * 50);
         }
 
-        private void OnTriggerEnter2D(Collider2D other)
+        private void OnCollisionEnter2D(Collision2D other)
         {
-            if (!other.CompareTag("Player"))
+            if (!other.gameObject.CompareTag("Player"))
             {
                 return; 
             }
 
-            var player = other.GetComponent<SocketPlayer>();
+            var player = other.gameObject.GetComponent<SocketPlayer>();
 
             if (player == null)
             {
                 return;
             }
-            
+
             if (!player.IsLocalPlayer)
             {
                 return;
             }
-            
-            // Pick up the soap bottle
+
+            Destroy(gameObject);
         }
     }
 }
